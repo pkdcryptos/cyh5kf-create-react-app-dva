9327780a9be334ff253140d583f07dfafab854cd
chenyu <chenyu@mchz.com.cn>
2019-07-10 15:41:42 +0800

    fix

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,17 +1,17 @@
 const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
-  app.use(proxy('/house', { target: 'https://evaluatepre.jd.com' }));
-  app.use(proxy('/vehicle', { target: 'https://evaluatepre.jd.com' }));
   app.use(
-    proxy("/dqjh/*", {
+    '/dqjh/*',
+    proxy({
         target: 'http://192.168.40.10:8080', // 王华峰
         // target: 'http://192.168.40.5:8080', // 沈韬
+        // target: 'http://192.168.40.32:8090', // 王晨旭
         secure: false,
         pathRewrite: {
           '^/dqjh': ''
-        }
+        },
     })
-  );
+  )
   
 };
